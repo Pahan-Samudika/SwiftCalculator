850c773115af1880d61c1e7dc2c36e73314463b9
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2023-12-12 18:18:13 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Pahan Abhayawardhane on 2023-12-12.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
